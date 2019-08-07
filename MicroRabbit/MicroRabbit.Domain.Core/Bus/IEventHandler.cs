b5e775ea524eb44f9a3c0ad6e08195524e3bfeb5
Shubham Shukla <shubham.shukla@citrix.com>
2019-08-07 17:51:31 -0400

    Defined the Domain Core Entities Interfaces

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent>: IEventHandler
+        where TEvent: Event
+    {
+        //This EventHandler takes in any event with a contraint that it should be of Type Event and implements IEventHandler
+
+        //Interface has a task handle that handles any Event
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
