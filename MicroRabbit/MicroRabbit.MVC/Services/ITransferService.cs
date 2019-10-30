03a995b664f9708dcb8f6c4fe6be1d72e3655bc0
Shubham Shukla <shubham.shukla@citrix.com>
2019-10-30 16:59:00 -0400

    Completed the MVC client and connected to the microservices.

---
+++ MicroRabbit/MicroRabbit.MVC/Services/ITransferService.cs
@@ -0,0 +1,14 @@
+﻿using MicroRabbit.MVC.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.MVC.Services
+{
+    public interface ITransferService
+    {
+        Task Transfer(TransferDTO transferDto);
+
+    }
+}
